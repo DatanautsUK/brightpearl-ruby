fece38fd1de15df3f17958593d6f4198dbc47a68
Simon Brook <simon@datanauts.co.uk>
2016-04-28 23:54:30 +0100

    update gems - get ready for webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'brightpearl-ruby'
 
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+
 RSpec.configure do |c|
   c.seed = Time.now
 end
